cf6825ac75075c42c31dfba7309b1eab9a0004c8
Dmitri Mamrukov <dmitri.mamrukov@ya.ru>
2024-11-25 22:07:46 +0300

    Add logging.

--- app/src/routes/subscriptions.rs
+++ app/src/routes/subscriptions.rs
@@ -13,6 +13,18 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database.",
+        request_id
+    );
+
     match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -25,9 +37,20 @@ pub async fn subscribe(
     .execute(connection_pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            println!("Failed to execute query: {}", e);
+        Ok(_) => {
+            log::info!(
+                "request_id {} - New subscriber details have been saved in the database.",
+                request_id
+            );
+
+            HttpResponse::Ok().finish()
+        }
+        Err(err) => {
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                err
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
